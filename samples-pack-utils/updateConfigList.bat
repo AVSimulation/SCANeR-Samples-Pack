b0b0d83e0da92473bfe41fdeeaa74618d62d7faa
unknown <ladm@PP042.OKTAL>
2021-08-12 12:04:16 +0900

    initiate utils

---
+++ samples-pack-utils/updateConfigList.bat
@@ -0,0 +1,30 @@
+@ECHO OFF
+
+call :getabs "../../configuration.cfg"
+
+echo AUTO UPDATE 'configuration.cfg'
+echo This will add all folders begining by "./config/SAMPLE_" to "%abspath%"
+
+:choice
+set /P c=Are you sure you want to continue [Y/N]?
+if /I "%c%" EQU "Y" goto :update
+if /I "%c%" EQU "N" goto :cancel
+goto :choice
+
+:update
+for /f %%c IN ('dir "../config/" /a:d /b ^| findstr /b "SAMPLE_"') do (
+	echo adding %%c...
+	echo %%c = ${STUDIO_PATH}/SCANeRstudio_2021/config/%%c >> ../../configuration.cfg
+)
+echo successfully updated "%abspath%".
+pause
+goto :eof
+
+:cancel
+echo 'configuration.cfg' was not updated.
+pause
+goto :eof
+
+:getabs
+  SET abspath=%~f1
+  EXIT /B
